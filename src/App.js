a4795abe889218b297fa8c4292a21b716e7dbc96
phuctd <trandinhphuc040898@gmail.com>
2021-08-23 14:26:20 +0700

    hgdsd

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from '../Components/Footer';
+import Header from '../Components/Header';
+// import Contact from '../form/Contact';
+import Homepage from '../home/Homepage';
+// import About from '../about/About';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Homepage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
